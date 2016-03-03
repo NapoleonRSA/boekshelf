283a55a676e0ce41ed40aa73ef5dc999efd9c981
Daniel Schwartz <Daniel Schwartz>
2016-03-03 21:49:29 +0200

    Database Project

---
+++ grootboekrak/Controllers/BoekController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace grootboekrak.Controllers
+{
+    public class BoekController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
